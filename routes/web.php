629151a8847625dec9477ab040f6951907537c60
ayshnw <dumpme053@gmail.com>
2025-05-07 21:57:03 +0700

    memperbarui  route

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\productController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\DetailproductController;
-use App\Http\Controllers\viewAllController;
-use App\Http\Controllers\productController;
-use App\Http\Controllers\CartController;
 use App\Http\Controllers\AboutController;
-use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\DetailproductController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,10 +27,12 @@ Route::get('/category', [ProductController::class, 'tampilKategori'])->name('tam
 Route::get('profile', function () {
     return view('pages/profile');
 });
+Route::get('/profile', [ProfileController::class, 'profile'])->name('profile');
 
 Route::get('about', function () {
     return view('pages/about');
 });
+Route::get('/about', [AboutController::class, 'about'])->name('about');
 
 Route::get('contact', function () {
     return view('pages/contact');
@@ -38,6 +42,8 @@ Route::get('/login', [loginController::class, 'tampilLogin'])->name('login');
 Route::get('/register', [RegisterController::class, 'tampilRegister'])->name('register');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::post('/contact/send', [ContactController::class, 'send'])->name('contact.send');
+
 Route::get('/detail_product', [DetailproductController::class, 'detail'])->name('detail_product');;
+Route::post('/cart/add', [ProductController::class, 'addToCart'])->name('cart.add');
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
 Route::get('/checkout', [CartController::class, 'checkout'])->name('checkout');
