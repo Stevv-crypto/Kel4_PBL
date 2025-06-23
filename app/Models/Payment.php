a2d7d5abfb2b839392d9845e42c9340accdcefee
Fahmii017 <fahmiahmad310705@gmail.com>
2025-06-23 23:11:51 +0700

    Update model Payment: relasi ke order

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,20 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Payment extends Model
 {
     protected $fillable = [
-        'category',
-        'method_name',
-        'account_name',
-        'account_number',
-        'logo_path',
+        'category', 'method_name', 'account_name', 'account_number', 'logo_path'
     ];
 
-    // Relasi: satu metode pembayaran bisa digunakan di banyak order
-    public function orders(): HasMany
+    public function orders()
     {
         return $this->hasMany(Order::class);
     }
